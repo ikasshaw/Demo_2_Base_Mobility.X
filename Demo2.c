fc7cd2321d801c73d1ee102850387ae0d8e70e8f
Isaac Shaw <83383820+ikasshaw@users.noreply.github.com>
2021-10-18 18:36:35 -0600

    Working for demo 2

--- Demo2.c
+++ Demo2.c
@@ -4,18 +4,18 @@
 #define PERIOD1 OC1RS
 #define dutyCycle2 OC2R
 #define PERIOD2 OC2RS
-#define mrDir _RA1
+#define mrDir _RB1
 #define mlDir _RB9
 #define mrON _RA0
 #define mlON _RB8
 #define CW 1
 #define CCW 0
 
-#pragma config FNOSC = FRCDIV
+#pragma config FNOSC = LPRC
 
 int steps1 = 0;
 int steps2 = 0;
-float wheelDiameter = 3;
+float wheelDiameter = 3.5;
 
 void config_pwmR(float period, float dutyCycle, int direction); // Sets up PWM #1 with a specified period
 void config_pwmL(float period, float dutyCycle, int direction); // Sets up PWM #2 with a specified period
@@ -29,87 +29,87 @@ void __attribute__((interrupt, no_auto_psv)) _OC1Interrupt(void) {
 
     if (steps1 >= 0) {
         ++steps1;
-        
-        
-        //After 4 Revolutions prepare for turn
-        
-        //Shut motor down
-        if (steps1 == 1600) {
-            mrON = 1; //Shut motor off before changing directions
-            
-        }
-        
-        //Changes direction of motor to execute turn
-        if (steps1 == 1650) {
-            mrDir = CCW; //Change direction 
-            mrON = 0; //Turn motor on again
-        }
-        
-        //When motor executes full turn change direction again to continue moving forward;
-        
-        //Shut motor down
-        if (steps1 == 2700) {
-            
-            mrON = 1; // Shut motor off
-        }
-        
-        //Change direction of motor to drive straight
-        if (steps1 == 2649) {
-            mrDir = CW; //Change direction 
-            mrON = 0; //Turn motor on
-            
-        }
-        
-        if (steps1 == 4000) {
-            
-            steps1 = -1;
-            mrON = 1; // Turn motor off
-        }
-    }
+            //After 4 Revolutions prepare for turn
     
-    //Arc length is S = r * Theta where theta is in radians
-    // S = 5*pi/2 ~ 7.86 inches
-    // Number of revolutions of wheel: R = S/D ~ 2.62 revolutions
+            //Shut motor down
+            if (steps1 == 400) {
+                mlON = 1; //Shut motor off before changing directions
+    
+            }
+    
+            
+            //Leaves motor off for 50 counts before executing turn
+            if (steps1 == 450) {
+                
+                mlON = 0; //Turn motor on again
+                
+            }
+    
+            //Shut motor down
+            if (steps1 == 720) {
+    
+                mlON = 1; // Shut motor off
+            }
+    
+            //Leaves motor off for 50 counts before moving again
+            if (steps1 == 770) {
+                mlON = 0; //Turn motor on
+    
+            }
+        
+        if (steps1 == 1080) {
+            steps1 = -1;
+            mlON = 1;
+        }
+    
+    }
     
 }
-void __attribute__((interrupt, no_auto_psv)) _OC2Interrupt(void) {
+void __attribute__((interrupt, no_auto_psv)) _OC2Interrupt(void)
+{
+    _OC2IF = 0; // Clear OC2 interrupt flag
 
-    _OC2IF = 0; // Clear OC1 interrupt flag
+    if (steps1 >= 0) {
 
-    if (steps2 >= 0) {
-        
-        ++steps2;
-
-        //After 4 Revolutions prepare for turn
+        //After 2 Revolutions prepare for turn
 
         //Shut motor down
-        if (steps2 == 1600) {
-            mlON = 1; //Shut motor off before changing directions
+        if (steps1 == 400) {
+            mrON = 1; //Shut motor off before changing directions
 
         }
 
-        if (steps2 == 1650) {
-            
-            mlON = 0; //Turn motor on again
-            
+        //Changes direction of motor to execute turn
+        if (steps1 == 450) {
+            mrDir = 0; //Change direction 
+            mrON = 0; //Turn motor on again
         }
+
+        //When motor executes full turn change direction again to continue moving forward;
 
         //Shut motor down
-        if (steps2 == 2649) {
+        if (steps1 == 720) {
 
-            mlON = 1; // Shut motor off
+            mrON = 1; // Shut motor off
         }
 
-        if (steps2 == 2700) {
-            mlON = 0; //Turn motor on
+        //Change direction of motor to drive straight
+        if (steps1 == 770) {
+            mrDir = 1; //Change direction 
+            mrON = 0; //Turn motor on
 
         }
-        if (steps2 == 4000) {
 
-            steps2 = -1;
-            mlON = 1; // Turn motor off
-        }
+
     }
+    else {
+        
+        mrON = 1;
+    }
+
+    //Arc length is S = r * Theta where theta is in radians
+    // S = 5*pi/2 ~ 7.86 inches
+    // Number of revolutions of wheel: R = S/(pi*D) ~ .7148 revolutions == 286 steps
     
 }
 
@@ -120,11 +120,11 @@ int main()
 //    _RCDIV = 0b101;
     
     TRISA = 0x0000; //Sets all outputs
-    TRISB = 0x0002; //Sets all outputs except B1
+    TRISB = 0x0000; //Sets all outputs except B1
     ANSB = 0x0000; //Sets all digital
 
-    config_pwmR(9999, .5 , CW);
-    config_pwmL(9999, .5, CCW);
+    config_pwmR(85, .5 , CW);
+    config_pwmL(85, .5, CCW);
 
 
     while (1);
@@ -135,32 +135,12 @@ int main()
 void config_pwmR(float period, float dutyCycle, int direction)
 {
 
-    PERIOD1 = period;
-    dutyCycle1 = 312;
-    mrDir = direction;
-
-    OC1CON1 = 0;
-
-    // Configure OC1
-    OC1CON1bits.OCTSEL = 0b111; // System (peripheral) clock as timing source
-    OC1CON2bits.SYNCSEL = 0x1F; // Select OC1 as synchronization source
-    OC1CON2bits.OCTRIG = 0; // Synchronizes with OC1 source instead of triggering with the OC1 source
-    OC1CON1bits.OCM = 0b110; // Edge-aligned PWM mode
-    
-    _OC1IP = 4; // Select OCx interrupt priority
-    _OC1IE = 1; // Enable OCx interrupt
-    _OC1IF = 0; // Clear OCx interrupt flag
-
-}
-
-void config_pwmL(float period, float dutyCycle, int direction)
-{
-
     PERIOD2 = period;
-    dutyCycle1 = PERIOD2*dutyCycle;
-    mlDir = direction;
-    
-    OC1CON1 = 0;
+    dutyCycle2 = (int)(PERIOD2*dutyCycle);
+    mrDir = direction;
+    mrON = 0;
+
+    OC2CON1 = 0;
 
     // Configure OC1
     OC2CON1bits.OCTSEL = 0b111; // System (peripheral) clock as timing source
@@ -174,20 +154,27 @@ void config_pwmL(float period, float dutyCycle, int direction)
 
 }
 
+void config_pwmL(float period, float dutyCycle, int direction)
+{
 
-//void updateMotorR(float dutyCycle, int direction) {
-//    
-//    
-//}
-//
-//void updateMotorL(float dutyCycle, int direction) {
-//
-//    
-//    
-//}
-/*
- 
- 
+    PERIOD1 = period;
+    dutyCycle1 = (int)(PERIOD1*dutyCycle);
+    mlDir = direction;
+    
+    OC1CON1 = 0;
+
+    // Configure OC1
+    OC1CON1bits.OCTSEL = 0b111; // System (peripheral) clock as timing source
+    OC1CON2bits.SYNCSEL = 0x1F; // Select OC1 as synchronization source
+    OC1CON2bits.OCTRIG = 0; // Synchronizes with OC1 source instead of triggering with the OC1 source
+    OC1CON1bits.OCM = 0b110; // Edge-aligned PWM mode
+    
+    _OC1IP = 4; // Select OCx interrupt priority
+    _OC1IE = 1; // Enable OCx interrupt
+    _OC1IF = 0; // Clear OCx interrupt flag
+
+}
+ /*
  PERIOD CALCULATION:
  
  * Time per pulse = 1 / Frequency
